f8354602d9527aade61e8acf29ca728a1eeb0073
Noah Holloway <noahholloway23@gmail.com>
2025-04-14 08:45:44 -0600

    init header

--- RentalForm.vb
+++ RentalForm.vb
@@ -1,6 +1,13 @@
-﻿Option Explicit On
+﻿'Noah Holloway
+'Spring 2025
+'RCET 2265
+'Car Rental
+
+Option Explicit On
 Option Strict On
 Option Compare Binary
 Public Class RentalForm
+    Private Sub RentalForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
 End Class
